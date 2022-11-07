8af2bca5a8f29ed03cdf938bdf45e2b898651bb7
Carlos Dante <contato.carloscdante@gmail.com>
2022-11-07 17:07:30 -0300

    implemented achievement fetching

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -10,3 +10,27 @@ type XUIDLoginRequestReturn = {
     display_claims: object;
     expires_on: string;
 }
+
+type AchievementReturn = {
+    id: string;
+    serviceConfigId: string;
+    name: string;
+    titleAssociations: object[];
+    progressState: string;
+    progression: object;
+    mediaAssets: object;
+    platforms: string[];
+    isSecret: boolean;
+    description: string;
+    lockedDescription: string;
+    productId: string;
+    achievementType: string;
+    participationType: string;
+    timeWindow: unknown;
+    rewards: object[];
+    estimatedTime: string;
+    deeplink: string;
+    isRevoked: boolean;
+}
+
+type Handler = ((t: any) => {})
